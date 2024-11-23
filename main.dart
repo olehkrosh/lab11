fd6da29dcae2d8f62bfdd04230b61a0a3d02ddc9
olehkrosh <114574028+olehkrosh@users.noreply.github.com>
2024-11-24 01:51:11 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Dmytro\'s LAB 11 IPZ-34'),
+      home: const MyHomePage(title: 'Oleg\'s LAB 11 IPZ-34'),
     );
   }
 }
